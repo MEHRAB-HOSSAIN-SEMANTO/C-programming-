036e5c6b25c7ad7359d2b6b015181babe46d890b
MEHRAB-HOSSAIN-SEMANTO <semanto-25024016@aaub.edu.bd>
2025-11-25 13:22:21 +0600

    Add files via upload

---
+++ sem2.c
@@ -0,0 +1,419 @@
+#include<stdio.h>
+
+struct std{
+float ave4201;
+float cse4201;
+float hum4203;
+float math4203;
+float chem4201;
+
+float cse4202;
+float ave4202;
+float chem4202;
+float ase4202;
+
+};
+
+
+
+int main(){
+
+struct std marks;
+float grade1,grade2,grade3,grade4,grade5,grade6,grade7,grade8;
+scanf("%f",&marks.ave4201);
+printf("\n enter score for ave4201=%f ",marks.ave4201);
+
+scanf("%f",&marks.cse4201);
+printf("\n enter score for cse4201=%f ",marks.cse4201);
+
+scanf("%f",&marks.hum4203);
+printf("\n enter score for hum4203=%f ",marks.hum4203);
+
+scanf("%f",&marks.math4203);
+printf("\n enter score for math4203=%f ",marks.math4203);
+
+scanf("%f",&marks.chem4201);
+printf("\n enter score for chem4201=%f ",marks.chem4201);
+
+scanf("%f",&marks.cse4202);
+printf("\n enter score for cse4202=%f ",marks.cse4202);
+
+scanf("%f",&marks.ave4202);
+printf("\n enter score for ave4202=%f ",marks.ave4202);
+
+scanf("%f",&marks.chem4202);
+printf("\n enter score for chem4202=%f ",marks.chem4202);
+
+
+scanf("%f",&marks.ase4202);
+printf("\n enter score for ase4202=%f ",marks.ase4202);
+
+
+
+
+if (marks.ave4201>=80){
+    float grade8=4*3;
+
+}
+else if (80>marks.ave4201>75){
+    float grade8=3.75*3;}
+
+else if (75>marks.ave4201>70){
+    float grade8=3.5*3;
+
+}
+else if (70>marks.ave4201>65){
+    float grade8=3.25*3;
+    }
+
+else if (65>marks.ave4201>60){
+    float grade8=3*3;
+    }
+
+else if (60>marks.ave4201>55){
+    float grade8=2.75*3;
+    }
+
+    else if (55>marks.ave4201>50){
+    float grade8=2.5*3;
+    }
+
+
+    else if (50>marks.ave4201>45){
+    float grade8=2.25*3;
+    }
+
+    else if (45>marks.ave4201>40){
+    float grade8=2*3;
+    }
+
+
+
+    if (marks.cse4201>=80){
+    float grade1=4*3;
+
+}
+else if (80>marks.cse4201>75){
+    float grade1=3.75*3;
+}
+else if (75>marks.cse4201>70){
+    float grade1=3.5*3;
+
+}
+else if (70>marks.cse4201>65){
+    float grade1=3.25*3;
+    }
+
+else if (65>marks.cse4201>60){
+    float grade1=3*3;
+    }
+
+else if (60>marks.cse4201>55){
+    float grade1=2.75*3;
+    }
+
+    else if (55>marks.cse4201>50){
+    float grade1=2.5*3;
+    }
+
+
+    else if (50>marks.cse4201>45){
+    float grade1=2.25*3;
+    }
+
+    else if (45>marks.cse4201>40){
+    float grade1=2*3;
+    }
+
+
+
+
+
+    if (marks.hum4203>=80){
+    float grade2=4*3;
+
+}
+else if (80>marks.hum4203>75){
+    float grade2=3.75*3;
+}
+else if (75>marks.hum4203>70){
+    float grade2=3.5*3;
+
+}
+else if (70>marks.hum4203>65){
+    float grade2=3.25*3;
+    }
+
+else if (65>marks.hum4203>60){
+    float grade2=3*3;
+    }
+
+else if (60>marks.hum4203>55){
+    float grade2=2.75*3;
+    }
+
+    else if (55>marks.hum4203>50){
+    float grade2=2.5*3;
+    }
+
+
+    else if (50>marks.hum4203>45){
+    float grade2=2.25*3;
+    }
+
+    else if (45>marks.hum4203>40){
+    float grade2=2*3;
+    }
+
+
+
+
+
+
+
+
+    if (marks.math4203>=80){
+    float grade3=4*3;
+
+}
+else if (80>marks.math4203>75){
+    float grade3=3.75*3;
+}
+else if (75>marks.math4203>70){
+    float grade3=3.5*3;
+
+}
+else if (70>marks.math4203>65){
+    float grade3=3.25*3;
+    }
+
+else if (65>marks.math4203>60){
+    float grade3=3*3;
+    }
+
+else if (60>marks.math4203>55){
+    float grade3=2.75*3;
+    }
+
+    else if (55>marks.math4203>50){
+    float grade3=2.5*3;
+    }
+
+
+    else if (50>marks.math4203>45){
+    float grade3=2.25*3;
+    }
+
+    else if (45>marks.math4203>40){
+    float grade3=2*3;
+    }
+
+
+
+
+
+    if (marks.chem4201>=80){
+    float grade4=4*3;
+
+}
+else if (80>marks.chem4201>75){
+    float grade4=3.75*3;
+}
+else if (75>marks.chem4201>70){
+    float grade4=3.5*3;
+
+}
+else if (70>marks.chem4201>65){
+    float grade4=3.25*3;
+    }
+
+else if (65>marks.chem4201>60){
+    float grade4=3*3;
+    }
+
+else if (60>marks.chem4201>55){
+    float grade4=2.75*3;
+    }
+
+    else if (55>marks.chem4201>50){
+    float grade4=2.5*3;
+    }
+
+
+    else if (50>marks.chem4201>45){
+    float grade4=2.25*3;
+    }
+
+    else if (45>marks.chem4201>40){
+    float grade4=2*3;
+    }
+
+
+
+
+
+    if (marks.cse4202>=80){
+    float grade5=4*3;
+
+}
+else if (80>marks.cse4202>75){
+    float grade5=3.75*3;
+}
+else if (75>marks.cse4202>70){
+    float grade5=3.5*3;
+
+}
+else if (70>marks.cse4202>65){
+    float grade5=3.25*3;
+    }
+
+else if (65>marks.cse4202>60){
+    float grade5=3*3;
+    }
+
+else if (60>marks.cse4202>55){
+    float grade5=2.75*3;
+    }
+
+    else if (55>marks.cse4202>50){
+    float grade5=2.5*3;
+    }
+
+
+    else if (50>marks.cse4202>45){
+    float grade5=2.25*3;
+    }
+
+    else if (45>marks.cse4202>40){
+    float grade5=2*3;
+    }
+
+
+
+
+    if (marks.ave4202>=80){
+    float grade6=4*3;
+
+}
+else if (80>marks.ave4202>75){
+    float grade6=3.75*3;
+}
+else if (75>marks.ave4202>70){
+    float grade6=3.5*3;
+
+}
+else if (70>marks.ave4202>65){
+    float grade6=3.25*3;
+    }
+
+else if (65>marks.ave4202>60){
+    float grade6=3*3;
+    }
+
+else if (60>marks.ave4202>55){
+    float grade6=2.75*3;
+    }
+
+    else if (55>marks.ave4202>50){
+    float grade6=2.5*3;
+    }
+
+
+    else if (50>marks.ave4202>45){
+    float grade6=2.25*3;
+    }
+
+    else if (45>marks.ave4202>40){
+    float grade6=2*3;
+    }
+
+
+
+    if (marks.chem4202>=80){
+    float grade6=4*3;
+
+}
+else if (80>marks.chem4202>75){
+    float grade6=3.75*3;
+}
+else if (75>marks.chem4202>70){
+    float grade6=3.5*3;
+
+}
+else if (70>marks.chem4202>65){
+    float grade6=3.25*3;
+    }
+
+else if (65>marks.chem4202>60){
+    float grade6=3*3;
+    }
+
+else if (60>marks.chem4202>55){
+    float grade6=2.75*3;
+    }
+
+    else if (55>marks.chem4202>50){
+    float grade6=2.5*3;
+    }
+
+
+    else if (50>marks.chem4202>45){
+    float grade6=2.25*3;
+    }
+
+    else if (45>marks.chem4202>40){
+    float grade6=2*3;
+    }
+
+
+
+
+
+    if (marks.ase4202>=80){
+    float grade7=4*3;
+
+}
+else if (80>marks.ase4202>75){
+    float grade7=3.75*3;
+}
+else if (75>marks.ase4202>70){
+    float grade7=3.5*3;
+
+}
+else if (70>marks.ase4202>65){
+    float grade7=3.25*3;
+    }
+
+else if (65>marks.ase4202>60){
+    float grade7=3*3;
+    }
+
+else if (60>marks.ase4202>55){
+    float grade7=2.75*3;
+    }
+
+    else if (55>marks.ase4202>50){
+    float grade7=2.5*3;
+    }
+
+
+    else if (50>marks.ase4202>45){
+    float grade7=2.25*3;
+    }
+
+    else if (45>marks.ase4202>40){
+    float grade7=2*3;
+    }
+
+
+
+
+    printf("total credit :19.50");
+
+
+float grade=grade1+grade2+grade3+grade4+grade5+grade6+grade7+grade8;
+
+
+    printf("\ntotal grade point=%f",grade);
+    return 0;
+}
